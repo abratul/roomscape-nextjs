123c0a4a07d469acaf59bd344fcd5e28f9eb7633
Alex B <alexbratuu@gmail.com>
2025-10-13 16:45:14 +0300

    hotfix: replacing the old project id with the new project id

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'vhznxrpzwfkwzrikhwcv.supabase.co',
+        hostname: 'anhhdfcpmpsnjzinkvwn.supabase.co',
       },
     ],
   },
